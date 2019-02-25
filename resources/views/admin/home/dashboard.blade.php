c22bddbbb5f84bf56679ecfaa64c95fe6303b5f3
Shahid Umar <shahid.umar@gmail.com>
2019-02-25 17:30:35 +0500

    site fix

--- resources/views/admin/home/dashboard.blade.php
+++ resources/views/admin/home/dashboard.blade.php
@@ -5,7 +5,7 @@
       $( document ).ready(function() {
         swal("Success", "{{session('success')}}", "success");
       });
-      
+
     </script>
 @endif
 @if(session('failed'))
@@ -13,7 +13,7 @@
       $( document ).ready(function() {
         swal("Failed", "{{session('failed')}}", "error");
       });
-      
+
     </script>
 @endif
 <?php
@@ -25,70 +25,10 @@
 <div class="row">
 
 
-
-<!-- Statistics begins-->
-<div class="row">
-          <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-object-group"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Admins</span>
-              <span class="info-box-number">45</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-red"><i class="fa fa-hourglass-o"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Users</span>
-              <span class="info-box-number">78</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-
-        <!-- fix for small devices only -->
-        <div class="clearfix visible-sm-block"></div>
-
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-green"><i class="fa fa-calendar"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Blocks</span>
-              <span class="info-box-number">71</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
-            <span class="info-box-icon bg-yellow"><i class="fa fa-file-audio-o"></i></span>
-
-            <div class="info-box-content">
-              <span class="info-box-text">Tasks</span>
-              <span class="info-box-number">36</span>
-            </div>
-            <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-      </div>
-<!-- Statistics ends -->
+<h1>Data will be available soon.</h1>
 
 
- 
+
 
 
     </div>
@@ -104,4 +44,4 @@
 @endcan
 @endpush
 
-@stop 
+@stop
